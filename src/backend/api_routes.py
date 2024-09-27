3c5459b2c3e1d0f1932a40086fc69843e00b2f01
iitwistii <ali_kashash@hotmail.co.uk>
2024-09-27 20:55:29 +0200

    fix SBAR display on chat box

--- src/backend/api_routes.py
+++ src/backend/api_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, session
 from gpt_service import generate_patient_case, load_diagnoses, generate_sbar_report
 
 api_routes = Blueprint('api', __name__)
@@ -16,11 +16,24 @@ def chat_init():
 @api_routes.route('/generate-case', methods=['GET'])
 def generate_case():
     # Call GPT model to generate patient case
-    json_file_path = '../../media\icd10_diagnoses.json'  # Adjust the path as necessary
+    json_file_path = '../../media/icd10_diagnoses.json'  # Adjust the path as necessary
     diagnoses = load_diagnoses(json_file_path)
     
     case = generate_patient_case(diagnoses)
+    # session['patient_case'] = case  # Store the case in the session
     
     sbar = generate_sbar_report(case)
 
     return jsonify(sbar)
+
+# @api_routes.route('/chat/message', methods=['POST'])
+# def chat_message():
+#     user_input = request.json.get('user_input')
+#     patient_case = session.get('patient_case')  # Retrieve the patient case from session
+
+#     if patient_case:
+#         response = chat_with_patient(user_input, patient_case)
+#     else:
+#         response = {"response": "No patient case found."}
+
+#     return jsonify(response)
